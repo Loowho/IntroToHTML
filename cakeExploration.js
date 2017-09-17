fa54002b328451b93ed47129b27bdefa4e0fc1a3
Loowho <cindy.wood@libertymutual.com>
2017-09-17 17:01:51 -0400

    Completed Javascript through section 5

---
+++ cakeExploration.js
@@ -0,0 +1,14 @@
+// Check to see if the user likes cake
+var likesCake = confirm("Do you like cake?");
+// If the user likes cake, 
+// Ask for their favorite cake
+if (likesCake) {
+    var favoriteCake = prompt("What is your favorite cake?");
+    // Alert them that their favorite cake is 
+    // whatever they inputted 
+    // from the prompt() function
+    alert("Your favorite cake is: " + favoriteCake);
+} else {
+    // If the user doesn't like cake, show an alert that the cake is a lie
+    alert("The cake is a lie anyhow.");
+}
